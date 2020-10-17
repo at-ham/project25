bf804efbbb0a0aa4ffec93e5cbfedf24d43fda9a
at-ham <69065580+at-ham@users.noreply.github.com>
2020-10-16 22:59:14 -0700

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class paper{
+constructor(x,y,r){
+    var options ={
+        
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+this.image=loadImage("paper.png")
+this.x=x;
+this.y=y;
+this.r=r;
+this.body=Bodies.circle(this.x,this.y,this.r,options);
+World.add(world,this.body)
+}
+display(){
+var pos=this.body.position;
+push()
+translate(pos.x,pos.y);
+imageMode(CENTER);
+image(this.image,0,0,2*this.r,2*this.r);
+pop()
+
+}
+
+
+}
